a267d05233e1f0375d5a536161b3f11d05779290
Bui Lam Quang Ngoc <bui.lam.quang.ngoc@framgia.com>
2019-03-15 23:20:11 +0700

    Chapter 3: Mostly static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = extract_locale_from_tld || I18n.default_locale
+  end
+
+  def extract_locale_from_tld
+    parsed_locale = request.host.split(".").last
+    locales = I18n.available_locales.map(&:to_s)
+    locales.include?(parsed_locale) ? parsed_locale : nil
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
+
+  def hello
+    render html: "Hello, world!"
+  end
 end
